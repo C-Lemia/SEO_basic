e074a4b58c533ac7ea380c08ac97dab484760700
Camila Lima <64974805+C-Lemia@users.noreply.github.com>
2024-10-16 12:55:11 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,33 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Função para extrair metadados e títulos SEO de uma página
+#### Função para extrair metadados e títulos SEO de uma página
 def verificar_seo(url):
-    # Fazer requisição HTTP para obter o conteúdo da página
+    #### Fazer requisição HTTP para obter o conteúdo da página
     response = requests.get(url)
     
-    # Parsear o conteúdo HTML com BeautifulSoup
+    # ###Parsear o conteúdo HTML com BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Extrair o título da página
+    # ###Extrair o título da página
     title = soup.find('title').string if soup.find('title') else 'Sem título'
 
-    # Extrair meta descrição
+    #### Extrair meta descrição
     description = soup.find('meta', attrs={'name': 'description'})
     description_content = description['content'] if description else 'Sem meta descrição'
 
-    # Extrair a tag H1
+    #### Extrair a tag H1 - se quiser pode incluir h2,h3...
     h1 = soup.find('h1')
     h1_text = h1.get_text() if h1 else 'Sem H1'
 
-    # Retornar os resultados
+    # Retornar os resultados###
     return {
         'Título': title,
         'Meta Descrição': description_content,
         'H1': h1_text
     }
 
-# Testar com uma URL
+# ###Testar com uma URL
 url = 'https://folha.qconcursos.com/'
 seo_data = verificar_seo(url)
 
